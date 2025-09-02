a1cd053c7f25cd0604c8f60581fd71b2531a84b7
SHIN MYEONG CHEOL <shin3481@gmail.com>
2025-09-02 16:25:54 +0900

    App update

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -3,8 +3,10 @@ import PropTypes from 'prop-types';
 import './TodoItem.css';
 
 class TodoItem extends Component {
-    //true 리턴 (checked 변수에 변경이 있는 경우 )이면 render()함수가 호출됨
-    //flase 리턴(checked 변수에 변경이 없는 경우 )이면 render()함수가 호출되지 않음(렌더링 생략)
+    /*
+        true 리턴 (checked 변수에 변동이 있는 경우)이면 render() 함수가 호출됨
+        false 리턴 (checked 변수에 변동이 없는 경우)이면 render() 함수가 호출되지 않음(렌더링 생략)
+    */    
     shouldComponentUpdate(nextProps, nextState) {
         return this.props.checked !== nextProps.checked;
     }
