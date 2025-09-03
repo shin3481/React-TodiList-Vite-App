f5380d0223fa34e159005d147038b95043777e8f
SHIN MYEONG CHEOL <shin3481@gmail.com>
2025-09-03 09:30:36 +0900

    reduce add

--- src/action/index.js
+++ src/action/index.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { PayloadAction } from '../../node_modules/@reduxjs/toolkit/dist/index.d';
 
 const BASE_URL = import.meta.env.VITE_API_URL;
 const apiUrl = `${BASE_URL}/todos`;
@@ -8,16 +7,16 @@ const apiUrl = `${BASE_URL}/todos`;
 export const FETCH_TODOS = "FETCH_TODOS";
 
 //TodoList Action 함수
-export const fetchALLTodos = () => {
-    return(dispatch) => {
+export const fetchAllTodos = () => {
+    return (dispatch) => {
         axios.get(apiUrl) //Promise
-        .then(res => dispatch({
-            type: FETCH_TODOS,
-            Payload: res.data
-        }))
-        .catch(error => {
-            console.log(error);
-            throw error;
-        });
+            .then(res => dispatch({
+                type: FETCH_TODOS,
+                payload: res.data
+            }))
+            .catch(error => {
+                console.log(error);
+                throw error;
+            });
     }
-};//fetchALLTodos
+}; //fetchAllTodos
